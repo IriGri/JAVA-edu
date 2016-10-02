52a6ecaaba6c24cf6f5c9df39f980584be5dd792
IriGri <igritsaj@MacBook-Air-Iri.local>
2016-10-02 20:52:29 +0300

    реализована проверка и обеспечение выполнения предусловий для изменения и удаления групп

--- addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,7 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreationTest() {
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupsHelper().initGroupCreation();
-        app.getGroupsHelper().fillGroupForm(new GroupData("test8", null, null));
-        app.getGroupsHelper().submitGroupCreation();
-        app.getGroupsHelper().returnToGroupPage();
+        app.getGroupsHelper().createGroup(new GroupData("test8", null, null));
     }
 
 }
--- addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package edu.stqa.irigri.addressbook.tests;
 
+import edu.stqa.irigri.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -7,6 +8,9 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletionTests() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupsHelper().isThereAGroup()) {
+            app.getGroupsHelper().createGroup(new GroupData("test8", null, null));
+        }
         app.getGroupsHelper().selectGroup();
         app.getGroupsHelper().deleteSelectedGroups();
         app.getGroupsHelper().returnToGroupPage();
