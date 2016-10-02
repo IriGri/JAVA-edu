e59eb7ce14524fcdc945d671d96006e53d6d2b8d
IriGri <igritsaj@MacBook-Air-Iri.local>
2016-10-02 12:45:03 +0300

    оператор == заменен на метод equals

--- addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -23,12 +24,12 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.FIREFOX) {
+        if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
+        } else if (Objects.equals(browser, BrowserType.CHROME)) {
             System.setProperty("webdriver.chrome.driver", "/Users/igritsaj/Downloads/chromedriver");
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.SAFARI) {
+        } else if (Objects.equals(browser, BrowserType.SAFARI)) {
             wd = new SafariDriver();
         }
 
