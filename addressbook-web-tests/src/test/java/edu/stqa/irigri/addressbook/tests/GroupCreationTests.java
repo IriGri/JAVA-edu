257cc08680cde80008428e3ca558481efcbd89d5
IriGri <igritsaj@MacBook-Air-Iri.local>
2016-10-02 12:51:36 +0300

    реализована возможность оставлять дефолтные значения для полей

--- addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreationTest() {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupsHelper().initGroupCreation();
-        app.getGroupsHelper().fillGroupForm(new GroupData("test5", "test2", "test3"));
+        app.getGroupsHelper().fillGroupForm(new GroupData("test8", null, null));
         app.getGroupsHelper().submitGroupCreation();
         app.getGroupsHelper().returnToGroupPage();
     }
