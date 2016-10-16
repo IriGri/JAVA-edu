d88de527ba8a3e3e57a59a40b1aa723d8258d6f7
IriGri <igritsaj@gmail.com>
2016-10-16 11:49:47 +0300

    добавлен атрибут для сравнения списка контактов

--- addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/edu/stqa/irigri/addressbook/appmanager/ContactHelper.java
@@ -84,7 +84,7 @@ public class ContactHelper extends HelperBase {
 
     public List<ContactData> getContactsList() {
         List<ContactData> contacts = new ArrayList<ContactData>();
-        List<WebElement> elements = wd.findElements(By.cssSelector("tr.odd"));
+        List<WebElement> elements = wd.findElements(By.name("entry"));
         for (WebElement element : elements) {
             String name = element.getText();
             ContactData contact = new ContactData(name, null, null, null, null, null);
